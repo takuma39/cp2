085921c2878a14f2c2fb51d624b692c4c0b58076
takuma39 <takuma.sato.job@gmail.com>
2022-10-18 05:48:46 +0900

    Add Header追加

--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
+// import TheWelcome from "@/components/TheWelcome.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
   </main>
 </template>
+
+<style lang="scss">
+@import "@/assets/sass/app2.scss";
+main {
+  height: 200vh;
+  background-color: rgb(163, 247, 148);
+}
+</style>
