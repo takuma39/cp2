fd9cc1eabb1fe8f3240e3c39e80e2730f736ffcc
takuma39 <takuma.sato.job@gmail.com>
2022-11-01 01:37:39 +0900

    Add SubHeroLayout

--- src/views/Work/WorkView.vue
+++ src/views/Work/WorkView.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
 // import MainHero from "./components/MainHeroComponent.vue";
+import SubHero from "@/layouts/SubHeroLayout.vue";
 import Contact from "@/layouts/ContactLink.vue";
 </script>
 
 <template>
   <main>
+    <SubHero en="work" ja="制作実績" />
     <Contact />
   </main>
 </template>
 
 <style lang="scss">
 @import "@/assets/sass/app2.scss";
+
 </style>
