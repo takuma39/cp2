6ab4ab78e833ef1f0ae1c7bcf6583fe5ddd0c408
takuma39 <takuma.sato.job@gmail.com>
2022-11-01 00:10:32 +0900

    Fix Router , Add WorkView

---
+++ src/views/Work/WorkView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+// import MainHero from "./components/MainHeroComponent.vue";
+import Contact from "@/layouts/ContactLink.vue";
+</script>
+
+<template>
+  <main>
+    <Contact />
+  </main>
+</template>
+
+<style lang="scss">
+@import "@/assets/sass/app2.scss";
+</style>
