720fce24058433d52e4229b4d512a8f2b0b9f136
takuma39 <takuma.sato.job@gmail.com>
2022-11-01 16:35:55 +0900

    Add BreadcrumbLatout

--- src/views/Work/WorkView.vue
+++ src/views/Work/WorkView.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
-// import MainHero from "./components/MainHeroComponent.vue";
 import SubHero from "@/layouts/SubHeroLayout.vue";
+import Breadcrumb from "@/layouts/BreadcrumbLatout.vue";
 import Contact from "@/layouts/ContactLink.vue";
+
+const breadcrumbs = [
+  {
+    name: "HOME",
+    path: "/",
+  },
+  {
+    name: "WORK",
+  },
+];
 </script>
 
 <template>
   <main>
     <SubHero en="work" ja="制作実績" />
+    <Breadcrumb :breadcrumbs="breadcrumbs" />
     <Contact />
   </main>
 </template>
