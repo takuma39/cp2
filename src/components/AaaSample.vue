86f1af45714076f850b8727f14f97477160683e2
takuma39 <takuma.sato.job@gmail.com>
2022-11-05 03:58:24 +0900

    Fix Sample

---
+++ src/components/AaaSample.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { RouterLink } from "vue-router";
+import Heading from "@/components/HeadingPrimaryComponent.vue";
+
+const props = defineProps({
+  count: {
+    type: Number,
+    required: false,
+  },
+});
+</script>
+<template>
+  <Heading>
+    <template v-slot:en>service</template>
+    <template v-slot:ja>事業内容</template>
+  </Heading>
+</template>
+
+<style scoped lang="scss">
+@import "@/assets/sass/app2.scss";
+
+</style>
