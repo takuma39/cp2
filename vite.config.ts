34772ef4c8e32971ba3c4a8ba64d5156315c9e3f
takuma39 <takuma.sato.job@gmail.com>
2022-11-22 03:01:27 +0900

    Fix config

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,10 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   // 本番時はgithubリポジトリをルートパスにする
-  // base: "/cp2",
-  // build: {
-  //   outDir: "docs",
-  // },
+  base: "/cp2",
+  build: {
+    outDir: "docs",
+  },
   plugins: [vue()],
   resolve: {
     alias: {
